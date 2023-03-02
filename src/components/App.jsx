1babfbc5867889bbd180bf659d52ee22fde2fd84
mvxseeds <me.semena@gmail.com>
2023-03-02 22:44:01 +0200

    Added functions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,13 +7,34 @@ export class App extends Component {
   state = {
     good: 0,
     neutral: 0,
-    bad: 0,
+    bad: 0
   };
 
+  onLeaveFeedback = e => {
+    this.setState(prevState => ({
+      [e.target.value]: prevState[e.target.value] + 1
+    }));
+  };
+
+  countTotalFeedback = () => {
+    const total = Object.values(this.state).reduce((acc, val) => acc + val, 0);
+    return total;
+  }
+
+  countPositiveFeedbackPercentage = () => {
+    const pos = Math.round((this.state.good / this.countTotalFeedback()) * 100);
+    return (pos) ? pos : 0;
+  }
+ 
   render() {
     return (
       <>
-        <FeedbackForm />
+        <FeedbackForm 
+          stats={this.state} 
+          onLeaveFeedback={this.onLeaveFeedback}
+          total={this.countTotalFeedback()}
+          positive={this.countPositiveFeedbackPercentage()}
+        />
         <ModernNormalize />
         <GlobalStyle />
       </>
--- src/components/FeedbackForm/FeedbackForm.jsx
+++ src/components/FeedbackForm/FeedbackForm.jsx
@@ -1,30 +1,25 @@
 import { Formik } from 'formik';
 import { Form, HeadingLabel, SubmitWrapper, InfoLabel, FeedbackButton } from './FeedbackForm.styled';
 
-export const FeedbackForm = () => {
+export const FeedbackForm = ({stats, onLeaveFeedback, total, positive}) => {
   return (
     <Formik
-      initialValues={{
-        good: 0,
-        neutral: 0,
-        bad: 0,
-      }}
-      onSubmit={values => {
-        console.log(values);
-      }}
+      initialValues={{...stats}}
     >
       <Form>
         <HeadingLabel>Please leave feedback</HeadingLabel>
         <SubmitWrapper>
-          <FeedbackButton type="submit">Good</FeedbackButton>
-          <FeedbackButton type="submit">Neutral</FeedbackButton>
-          <FeedbackButton type="submit">Bad</FeedbackButton>
+          <FeedbackButton type="button" value="good" onClick={onLeaveFeedback}>Good</FeedbackButton>
+          <FeedbackButton type="button" value ="neutral" onClick={onLeaveFeedback}>Neutral</FeedbackButton>
+          <FeedbackButton type="button" value ="bad" onClick={onLeaveFeedback}>Bad</FeedbackButton>
         </SubmitWrapper>
 
         <HeadingLabel>Statistics</HeadingLabel>
-        <InfoLabel>Good: </InfoLabel>
-        <InfoLabel>Neutral: </InfoLabel>
-        <InfoLabel>Bad: </InfoLabel>
+        <InfoLabel>Good: {stats.good}</InfoLabel>
+        <InfoLabel>Neutral: {stats.neutral}</InfoLabel>
+        <InfoLabel>Bad: {stats.bad}</InfoLabel>
+        <InfoLabel>Total: {total}</InfoLabel>
+        <InfoLabel>PositiveFeedback: {positive}%</InfoLabel>
       </Form>
     </Formik>
   );
